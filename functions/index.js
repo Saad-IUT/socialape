b9ee7eef71c7f392e6e07b58822097c670ecc320
Saad <saadbinjohir@gmail.com>
2020-05-17 12:20:47 +0600

    User Registration

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,26 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-
+const app = require("express")();
 admin.initializeApp();
 
-const express = require("express");
-const app = express();
+const config = {
+  apiKey: "AIzaSyCjIPQX8O_BQnYcCXL2HkWXYBa2SbP2xlU",
+  authDomain: "socialape-beb7e.firebaseapp.com",
+  databaseURL: "https://socialape-beb7e.firebaseio.com",
+  projectId: "socialape-beb7e",
+  storageBucket: "socialape-beb7e.appspot.com",
+  messagingSenderId: "141628790553",
+  appId: "1:141628790553:web:1798e8e64e3f4c41405788",
+  measurementId: "G-FXMGWX9ZGK",
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get("/screams", (req, res) => {
-  admin
-    .firestore()
-    .collection("screams")
+  db.collection("screams")
     .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
@@ -33,9 +44,7 @@ app.post("/scream", (req, res) => {
     userHandle: req.body.userHandle,
     createdAt: new Date().toISOString(),
   };
-  admin
-    .firestore()
-    .collection("screams")
+  db.collection("screams")
     .add(newScream)
     .then((doc) => {
       res.json({ message: `Document ${doc.id} created successfully` });
@@ -43,6 +52,54 @@ app.post("/scream", (req, res) => {
     .catch((err) => {
       res.status(500).json({ error: `something went wrong!` });
       console.error(err);
+    });
+});
+
+//Signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+  //Todo validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: `This handle is already taken` });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
     });
 });
 
