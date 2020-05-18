a52d216d39879fe52e3456b2c11fa9703f6bc7cc
Saad <saadbinjohir@gmail.com>
2020-05-18 23:46:59 +0600

    Add and Get User Profile Details

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -5,7 +5,11 @@ const config = require('../util/config')
 const firebase = require('firebase')
 firebase.initializeApp(config)
 
-const { validateSignupData, validateLoginData } = require('../util/validators')
+const {
+  validateSignupData,
+  validateLoginData,
+  reduceUserDetails,
+} = require('../util/validators')
 
 //Sign users up
 exports.signup = (req, res) => {
@@ -27,7 +31,7 @@ exports.signup = (req, res) => {
     .get()
     .then(doc => {
       if (doc.exists) {
-        return res.status(400).json({ handle: `This handle is already taken` })
+        return res.status(400).json({ handle: 'This handle is already taken' })
       } else {
         return firebase
           .auth()
@@ -94,6 +98,48 @@ exports.login = (req, res) => {
           .status(403)
           .json({ general: 'Wrong credentials, please try again' })
       } else return res.status(500).json({ error: err.code })
+    })
+}
+
+// Add user details
+exports.addUserDetails = (req, res) => {
+  let userDetails = reduceUserDetails(req.body)
+
+  db.doc(`/users/${req.user.handle}`)
+    .update(userDetails)
+    .then(() => {
+      return res.json({ message: 'Details added successfully' })
+    })
+    .catch(err => {
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    })
+}
+
+// Get own user details
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {}
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.credentials = doc.data()
+        return db
+          .collection('likes')
+          .where('userHandle', '==', req.user.handle)
+          .get()
+      }
+    })
+    .then(data => {
+      userData.likes = []
+      data.forEach(doc => {
+        userData.likes.push(doc.data())
+      })
+      return res.json(userData)
+    })
+    .catch(err => {
+      console.error(err)
+      return res.status(500).json({ error: err.code })
     })
 }
 
--- functions/index.js
+++ functions/index.js
@@ -5,16 +5,24 @@ const FBAuth = require('./util/fbAuth')
 const { db } = require('./util/admin')
 
 const { getAllScreams, postOneScream } = require('./handlers/screams')
-const { signup, login, uploadImage } = require('./handlers/users')
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require('./handlers/users')
 
 //Scream routes
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
+app.post('/user/image', FBAuth, uploadImage)
+app.post('/user', FBAuth, addUserDetails)
+app.get('/user', FBAuth, getAuthenticatedUser)
 
-//Users routes //Todo : Unique handle
+//Users routes
 app.post('/signup', signup)
 app.post('/login', login)
-app.post('/user/image', FBAuth, uploadImage)
 
 exports.api = functions.region('asia-east2').https.onRequest(app)
 
